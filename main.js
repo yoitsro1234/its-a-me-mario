566b9bd036cd61bca73cea4b160e9dc935cd0c28
yoitsro1234 <72605391+yoitsro1234@users.noreply.github.com>
2021-06-22 15:29:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 function preload() {
 	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
 	setSprites();
 	MarioAnimation();
 }
 
 function setup() {
 	canvas = createCanvas(1240,336);
+	canvas.parent("canvas");
 	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent("game_console");
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded(){
+	console.log("Mamma Mario, Model Initialized!");
+}
+
+function gotPoses(results){
+	if (results.length > 0){
+		rwx = results[0].pose.rightWrist.x;
+		rwy = results[0].pose.rightWrist.y;
+		console.log(results);
+	}
 }
 
 function draw() {
 	game()
 }
-
-
-
-
-
-
